da86b3822389f403fc54471a7cbcd2a8fa0d655f
ikesato <ikesato76@gmail.com>
2017-06-19 12:48:53 +0000

    implement

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const app = express();
 
 app.get('*', (req, res) => {
   console.log("GET", req.path);
-  res.send(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-  //res.send('hello world');
+  const r = fs.readFileSync('path/to/fake/dir/a.txt').toString();
+  res.send(r);
 });
 
 //app.listen(3000);
-
 module.exports = app;
--- index.js
+++ index.js
@@ -8,34 +8,9 @@ mock({
 
 request(app)
   .get('/')
-  //.expect(200)
+  .expect(200)
   .end(function(err, res) {
+    console.log(res.text)
+    mock.restore();
     if (err) throw err;
   });
-
-
-mock.restore();
-
-
-
-require('fs').readFileSync('/dev/stdin', 'utf8');
-
-//const fs = require('fs');
-
-//
-//
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//
-//fs.writeFileSync('path/to/fake/dir/a.txt', "hoge");
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//
-//mock({
-//  'path/to/fake/dir/a.txt': "hello world2"
-//});
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString());
-//
-//mock.restore();
-//console.log(fs.readFileSync('path/to/fake/dir/a.txt').toString()); // cause Error: ENOENT
